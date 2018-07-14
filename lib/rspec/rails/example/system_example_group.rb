3b1aa94f487519c21f9de8b4380a2705404ba846
ta1kt0me <p.wadachi@gmail.com>
2018-07-14 12:52:33 +0900

    Make puma log silence completely

--- lib/rspec/rails/example/system_example_group.rb
+++ lib/rspec/rails/example/system_example_group.rb
@@ -46,6 +46,10 @@ module RSpec
       end
 
       included do |other|
+        ActiveSupport.on_load(:action_dispatch_system_test_case) do
+          ActionDispatch::SystemTesting::Server.silence_puma = true
+        end
+
         begin
           require 'capybara'
           require 'action_dispatch/system_test_case'
@@ -70,10 +74,6 @@ module RSpec
         other.include BlowAwayAfterTeardownHook
 
         attr_reader :driver
-
-        if ActionDispatch::SystemTesting::Server.respond_to?(:silence_puma=)
-          ActionDispatch::SystemTesting::Server.silence_puma = true
-        end
 
         def initialize(*args, &blk)
           super(*args, &blk)
